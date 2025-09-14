4f4ba8ea9e846dcd7485b346bd022c9befccc958
isnikx0 <isaievnikitaa@gmail.com>
2025-09-14 17:12:34 +0200

    ываыаываыв

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const supabaseUrl = "https://fanwgzwdaeuxnqqnyhcf.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhbndnendkYWV1eG5xcW55aGNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NTk4ODIsImV4cCI6MjA3MzQzNTg4Mn0.SY3Z7G_3TyLt-9ZuIVnqes1tvxFzrrT3oE9nX8Cw-Nc"
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 function showSection(id) {
   document.querySelectorAll('section').forEach(s => s.classList.remove('active'));
   document.getElementById(id).classList.add('active');
